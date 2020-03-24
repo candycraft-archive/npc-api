9d8eab34e51081602bccac83b5f9f48a64552312
pauhull <pauhull3@gmail.com>
2020-03-24 21:43:45 +0100

    Removed PacketReader

--- plugin/src/main/java/de/pauhull/npcapi/NpcApi.java
+++ plugin/src/main/java/de/pauhull/npcapi/NpcApi.java
@@ -2,12 +2,10 @@ package de.pauhull.npcapi;
 
 import de.pauhull.npcapi.impl.NmsHelper;
 import de.pauhull.npcapi.listener.NpcListener;
-import de.pauhull.npcapi.packet.PacketReader;
 import de.pauhull.npcapi.v1_12_R1.NmsHelperImpl_v1_12_R1;
 import de.pauhull.npcapi.v1_8_R3.NmsHelperImpl_v1_8_R3;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.inventivetalent.packetlistener.PacketListenerAPI;
 
 /**
  * Created by Paul
@@ -35,7 +33,7 @@ public class NpcApi extends JavaPlugin {
         instance = this;
 
         new NpcListener(this);
-        PacketListenerAPI.addPacketHandler(new PacketReader());
+        //PacketListenerAPI.addPacketHandler(new PacketReader());
     }
 
     public NmsHelper getNmsHelper() {
@@ -45,16 +43,10 @@ public class NpcApi extends JavaPlugin {
 
         String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
 
-        switch (version) {
-            case "v1_8_R3":
-                nmsHelper = new NmsHelperImpl_v1_8_R3();
-                break;
-            case "v1_12_R1":
-                nmsHelper = new NmsHelperImpl_v1_12_R1();
-                break;
-            default: // fallback
-                nmsHelper = new NmsHelperImpl_v1_8_R3();
-                break;
+        if (version.equals("v1_12_R1")) {
+            nmsHelper = new NmsHelperImpl_v1_12_R1();
+        } else {
+            nmsHelper = new NmsHelperImpl_v1_8_R3();
         }
 
         return nmsHelper;
